39a2ef96fd6238bdc4b25e05210626f8d8db7715
jul003 <dzulkiflifaiz11@gmail.com>
2024-07-15 19:02:01 +0700

    getbyid

--- url/url.go
+++ url/url.go
@@ -28,5 +28,7 @@ func Web(page *fiber.App) {
 	page.Delete("/deletegadget/:id", controller.DeleteGadgetByID)
 	page.Get("/docs/*", swagger.HandlerDefault)
 
+	page.Get("/gadget:id", controller.GetGadgetByID)
+
 	
 }
